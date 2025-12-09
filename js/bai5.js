ccaa66ba3f824649447a053e7cf392590c1468f6
giang <ntg12072005@gmail.com>
2025-12-09 15:55:04 +0700

    ngay 3/12

---
+++ js/bai5.js
@@ -0,0 +1,10 @@
+function getTT() {
+  let name = document.getElementById("name").value;
+  let password = document.getElementById("password").value;
+  let city = document.getElementById("city").value;
+  console.log("Họ tên: " + name);
+  console.log("Mật khẩu: " + password);
+  console.log("Thành phố: " + city);
+}
+console.log(new Date());
+console.log("Số pi: " + Math.PI);
